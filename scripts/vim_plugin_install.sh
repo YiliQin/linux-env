5ce38bf68ac6b2bc74cafefb18e4f4fab2695b08
Yili Qin <yili.jrl@gmail.com>
2021-10-30 01:00:46 +0900

    [vim] set tab=2 and rename file vimrc

--- scripts/vim_plugin_install.sh
+++ scripts/vim_plugin_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install ctags
-cp $PWD/../vim/vimrc ~/.vimrc
+cp $PWD/../vim/vimrc.in ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # open vim
